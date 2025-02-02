dba4ea6a1616e66f7df51d8f077e01622e6a5c44
ifilot <ivo@ivofilot.nl>
2025-02-02 19:36:25 +0100

    Expanding mnemonic data

---
+++ scripts/optcodes/6502_optcodes.py
@@ -0,0 +1,36 @@
+from data import addressModeTable, opcodeTable
+
+def main():
+    
+    optcodes = []
+    mnemonics = {}
+    for i in range(0,256):
+        if i in opcodeTable.keys():
+            v = opcodeTable[i]
+            mode = [k for k in addressModeTable.keys()].index(v[2])
+            
+            optcodes.append('.byte "%s", $%02X, $%02X, $00, $%02X ' % (v[1].ljust(4, " ").upper(), v[0], mode, i))
+            
+            if v[1].upper() not in mnemonics.keys():
+                mnemonics[v[1].upper()] = []
+            mnemonics[v[1].upper()].append((mode, i))
+                
+        else:
+            optcodes.append('.byte "????", $01, $FF, $00, $%02X' % i)
+
+    mnemonics = dict(sorted(mnemonics.items()))
+    lines = []
+    for k,v in mnemonics.items():
+        lines.append('data_%s:' % k.lower())
+        data = ''
+        for mode in v:
+            data += '$%02X,' % mode[0]
+            data += '$%02X,' % mode[1]
+        data = data[:-1]
+        lines.append('    .byte %s' % data)
+    
+    for line in lines:
+        print(line)
+
+if __name__ == '__main__':
+    main()
