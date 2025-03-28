78059a1f83d8ae3a5aef3baf13e02e064233b6f4
ifilot <ivo@ivofilot.nl>
2025-03-28 22:00:33 +0100

    Revising TINY board and creating test ROM for TINY board

--- tests/io/io.s
+++ tests/io/io.s
@@ -1,86 +1,92 @@
-; this code is a simple monitor class for the ByteCradle 6502
+.export newline
+.export putstr
+.export getch
+.export putch
+.export putstrnl
+.export char2num
+.export char2nibble
+.export chartoupper
+.export puthex
+.export puthexword
+.export putdec
+.export putds
+.export putspace
+.export puttab
+.export printnibble
+.export ischarvalid
+.export clearline
 
 .PSC02
 
-.define ACIA_DATA       $7F04
-.define ACIA_STAT       $7F05
-.define ACIA_CMD        $7F06
-.define ACIA_CTRL       $7F07
-
-.define STRLB           $00
-.define STRHB           $01
-
-.define LF              $0A     ; LF character (line feed)
-.define ESC             $1B     ; VT100 escape character
-
-;-------------------------------------------------------------------------------
-; PROGRAM HEADER
-;-------------------------------------------------------------------------------
+.include "constants.inc"
 
 .segment "CODE"
 
 ;-------------------------------------------------------------------------------
-; Start sequence
+; GETCHAR routine
+;
+; retrieve a char from the key buffer in the accumulator
+; Garbles: A,X
 ;-------------------------------------------------------------------------------
-boot:                              ; reset vector points here
-    sei
-    ldx #$FF
-    txs
-    jsr init_system
-    jmp main
+getch:
+    phx
+    lda TBPL            ; load textbuffer left pointer
+    cmp TBPR            ; load textbuffer right pointer
+    beq @nokey          ; if the same, exit routine
+    ldx TBPL            ; else, load left pointer
+    lda TB,x            ; load value stored in text buffer
+    inc TBPL
+    jmp @exit
+@nokey:
+    lda #0
+@exit:
+    plx
+    rts
 
-main:
-    lda #<@str                     ; load lower byte
-    ldx #>@str                     ; load upper byte
-    jsr putstr
+;-------------------------------------------------------------------------------
+; NEWLINE routine
+;
+; Conserves: A
+;
+; print new line to the screen
+;-------------------------------------------------------------------------------
+newline:
+    sta BUF1
     lda #LF
+    jsr putch
+    lda #CR
+    jsr putch
+    lda BUF1
+    rts
+
+;-------------------------------------------------------------------------------
+; PUTSPACE routine
+;
+; print single space
+;-------------------------------------------------------------------------------
+putspace:
+    lda #' '
     jsr putch
     rts
 
-@str:
-    .asciiz "Hello World!"
-
 ;-------------------------------------------------------------------------------
-; Initialize the system
+; PUTDS routine
+;
+; print two spaces
 ;-------------------------------------------------------------------------------
-init_system:
-    jsr clear_zp
-    jsr init_acia
-    jsr init_screen
-    cli                 ; enable interrupts
+putds:
+    jsr putspace
+    jsr putspace
     rts
 
 ;-------------------------------------------------------------------------------
-; Clear the lower part of the zero page which is used by the BIOS
+; PUTTAB routine
+;
+; print four spaces
 ;-------------------------------------------------------------------------------
-clear_zp:
-    ldx #0
-@next:
-    stz $00,x
-    inx
-    bne @next
-    rts
-
-;-------------------------------------------------------------------------------
-; Initialize screen
-;-------------------------------------------------------------------------------
-init_screen:
-    lda #<@resetscroll
-    ldx #>@resetscroll
-    jsr putstr
-    rts
-
-@resetscroll:
-    .byte ESC,"[2J",ESC,"[r",0
-
-;-------------------------------------------------------------------------------
-; Initialize I/O
-;-------------------------------------------------------------------------------
-init_acia:
-    lda #$10		; use 8N1 with a 115200 baud
-    sta ACIA_CTRL	; write to ACIA control register
-    lda #%00001001  ; No parity, no echo, no interrupts.
-    sta ACIA_CMD	; write to ACIA command register
+puttab:
+    jsr putds
+    jsr putds
     rts
 
 ;-------------------------------------------------------------------------------
@@ -98,12 +104,240 @@ putstr:
 @skip:
     ldy #0
 @nextchar:
-    lda (STRLB),y       ; load character from string
-    beq @exit           ; if terminating character is read, exit
-    jsr putch           ; else, print char
-    iny                 ; increment y
-    jmp @nextchar       ; read next char
+    lda (STRLB),y   ; load character from string
+    beq @exit       ; if terminating character is read, exit
+    jsr putch      ; else, print char
+    iny             ; increment y
+    jmp @nextchar   ; read next char
 @exit:
+    rts
+
+;-------------------------------------------------------------------------------
+; PUTSTRNL routine
+; Garbles: A,X,Y
+; Input X:A contains HB:LB of string pointer
+;
+; Same as PUTSTR function, but puts a newline character at the end of the 
+; string.
+;-------------------------------------------------------------------------------
+putstrnl:
+    jsr putstr
+    lda #LF
+    jsr putch
+    rts
+
+;-------------------------------------------------------------------------------
+; PUTBACKSPACE routine
+; 
+; places the cursor one position back and wipes existing character
+;-------------------------------------------------------------------------------
+putbackspace:
+    lda #$08
+    jsr putch
+    lda #' '
+    jsr putch
+    lda #$08
+    jsr putch
+    rts
+
+;-------------------------------------------------------------------------------
+; CHAR2NUM routine
+;
+; convert characters stored in A,X to a single number stored in A;
+; sets C on an error, assume A contains high nibble and X contains low nibble
+;-------------------------------------------------------------------------------
+char2num:
+    jsr char2nibble
+    bcs @exit       ; error on carry set
+    asl a           ; shift left 4 bits to create higher nibble
+    asl a
+    asl a
+    asl a
+    sta BUF1        ; store in buffer on ZP
+    txa             ; transfer lower nibble from X to A
+    jsr char2nibble ; repeat
+    bcs @exit       ; error on carry set
+    ora BUF1        ; combine nibbles
+@exit:
+    rts
+
+;-------------------------------------------------------------------------------
+; CHAR2NIBBLE routine
+;
+; convert hexcharacter stored in a to a numerical value
+; sets C on an error
+;-------------------------------------------------------------------------------
+char2nibble:
+    cmp #'0'        ; is >= '0'?
+    bcc @error      ; if not, throw error 
+    cmp #'9'+1      ; is > '9'?
+    bcc @conv       ; if not, char between 0-9 -> convert
+    cmp #'A'        ; is >= 'A'?
+    bcc @error      ; if not, throw error
+    cmp #'F'+1      ; is > 'F'?
+    bcs @error      ; if so, throw error
+    sec
+    sbc #'A'-10     ; subtract
+    jmp @exit
+@conv:
+    sec
+    sbc #'0'
+    jmp @exit
+@error:
+    sec             ; set carry
+    rts
+@exit:
+    clc
+    rts
+
+;-------------------------------------------------------------------------------
+; ISCHARVALID routine
+;
+; Assess whether character stored in A is printable, i.e., lies between $20 and 
+; $7E (inclusive). If so, clear carry, else set the carry.
+;
+; Conserves: A, X, Y
+;-------------------------------------------------------------------------------
+ischarvalid:
+    cmp #$20                ; if less than $20, set carry flag and exit
+    bcc @invalid
+    cmp #$7F                ; compare with $7F, comparison yields desired result
+    rts
+@invalid:
+    sec
+    rts
+
+;-------------------------------------------------------------------------------
+; CLEARLINE routine
+;
+; Clears the current line on the terminal
+;-------------------------------------------------------------------------------
+clearline:
+    ldx #>@clearline
+    lda #<@clearline
+    jsr putstr
+    rts
+@clearline:
+    .byte ESC, "[2K", $0D, $00
+
+;-------------------------------------------------------------------------------
+; CHARTOUPPER
+;
+; Convert a character in A, if lowercase, to uppercase
+;-------------------------------------------------------------------------------
+chartoupper:
+    cmp #'a'
+    bcc @notlower
+    cmp #'z'+1
+    bcs @notlower
+    sec
+    sbc #$20
+@notlower:
+    rts
+
+;-------------------------------------------------------------------------------
+; PUTHEX routine
+;
+; Print a byte loaded in A to the screen in hexadecimal formatting
+;
+; Conserves:    A,X,Y
+; Uses:         BUF1
+;-------------------------------------------------------------------------------
+puthex:
+    sta BUF1
+    lsr a           ; shift right; MSB is always set to 0
+    lsr a
+    lsr a
+    lsr a
+    jsr printnibble
+    lda BUF1
+    and #$0F
+    jsr printnibble
+    lda BUF1
+    rts
+
+;-------------------------------------------------------------------------------
+; PUTHEXWORD routine
+;
+; Prints hexword to screen
+;-------------------------------------------------------------------------------
+puthexword:
+    sta BUF2    ; low byte
+    stx BUF3    ; high byte
+    lda BUF3
+    jsr puthex
+    lda BUF2
+    jsr puthex
+    rts
+
+;-------------------------------------------------------------------------------
+; PUTDEC routine
+;
+; Print a byte loaded in A to the screen in decimal formatting. Do not prepend
+; the number with any spaces nor with any zeros.
+;
+; Conserves:    A
+; Garbles:      X,Y
+; Uses:         BUF1,BUF2
+;-------------------------------------------------------------------------------
+putdec:
+    pha                 ; put A on the stack
+    ldx #1
+    stx BUF2
+    inx
+    ldy #$40
+@l1:
+    sty BUF1
+    lsr
+@l2:
+    rol
+    bcs @l3
+    cmp @vals,x
+    bcc @l4
+@l3:
+    sbc @vals,x
+    sec
+@l4:
+    rol BUF1
+    bcc @l2
+    tay
+    cpx BUF2
+    lda BUF1
+    bcc @l5
+    beq @l6
+    stx BUF2
+@l5:
+    eor #$30
+    jsr putch
+@l6:
+    tya
+    ldy #$10
+    dex
+    bpl @l1
+    pla                 ; retrieves A from the stack
+    rts
+
+@vals:
+    .byte 128,160,200
+
+;-------------------------------------------------------------------------------
+; PRINTNIBBLE routine
+;
+; Print the four LSB of the value in A to the screen; assumess that the four MSB
+; of A are already reset
+;
+; Garbles: A
+;-------------------------------------------------------------------------------
+printnibble:
+    cmp #10
+    bcs @alpha
+    adc #'0'
+    jmp @exit
+@alpha:
+    clc
+    adc #'A'-10
+@exit:
+    jsr putch
     rts
 
 ;-------------------------------------------------------------------------------
@@ -120,37 +354,15 @@ putstr:
 ;  4.000 MHz    :  69
 ;  5.120 MHz    :  89
 ; 10.000 MHz    : 174
-; 12.000 MHz    : 208 -> does not work
-; 14.310 MHz    : 249 -> does not work
+; 12.000 MHz    : 208 (works on Tiny)
+; 14.310 MHz    : 249 (works on Tiny)
 ;-------------------------------------------------------------------------------
 putch:
     pha             ; preserve A
     sta ACIA_DATA   ; write the character to the ACIA data register
-    lda #89         ; initialize inner loop
+    lda #249        ; initialize inner loop
 @inner:
     dec             ; decrement A; 2 cycles
     bne @inner      ; check if zero; 3 cycles
     pla             ; retrieve A from stack
     rts
-
-;-------------------------------------------------------------------------------
-; Interrupt Service Routine
-;
-; Checks whether a character is available over the UART and stores it in the
-; key buffer to be parsed later.
-;-------------------------------------------------------------------------------
-isr:
-    pha             ; put A on stack
-    lda ACIA_STAT   ; check status
-    and #$08        ; check for bit 3
-    beq isr_exit    ; if not set, exit isr
-    lda ACIA_DATA   ; load byte
-    jsr putch
-isr_exit:
-    pla             ; recover A
-    rti
-
-.segment "VECTORS"
-    .word boot          ; reset / $FFFA
-    .word boot          ; nmi / $FFFC
-    .word isr           ; irq/brk / $FFFE
